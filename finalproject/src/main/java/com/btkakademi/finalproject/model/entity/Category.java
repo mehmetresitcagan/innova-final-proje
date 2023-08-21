bd1731c6f2382cab3cf2944ba9479cd3168c8351
mehmetresitcagan@posta.mu.edu.tr <mehmetresitcagan@posta.mu.edu.tr>
2023-08-21 22:41:56 +0300

    initial commit

---
+++ finalproject/src/main/java/com/btkakademi/finalproject/model/entity/Category.java
@@ -0,0 +1,34 @@
+package com.btkakademi.finalproject.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "kategoriler")
+public class Category {
+    @Id
+    @Column(name = "kategori_id")
+    private int id;
+
+    @Column(name = "kategori_adi")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
