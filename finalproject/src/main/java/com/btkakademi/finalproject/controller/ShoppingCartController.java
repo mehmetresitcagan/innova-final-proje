1f368f5583516336effbd15ae309d1ee33f54af1
mehmetresitcagan@posta.mu.edu.tr <mehmetresitcagan@posta.mu.edu.tr>
2023-08-25 07:47:06 +0300

    07.46
    shoppingCart ve user controller dönüş objeleri düzeltildi.

--- finalproject/src/main/java/com/btkakademi/finalproject/controller/ShoppingCartController.java
+++ finalproject/src/main/java/com/btkakademi/finalproject/controller/ShoppingCartController.java
@@ -1,6 +1,9 @@
 package com.btkakademi.finalproject.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +23,8 @@ public class ShoppingCartController {
     @Autowired
     ShoppingCartService shoppingCartService;
 
+    private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
+
     // Belirtilen cartId'ye sahip alışveriş sepetini alır.
     @GetMapping("/{cartId}")
     public ShoppingCart getShoppingCartById(@PathVariable int cartId) {
@@ -31,25 +36,40 @@ public class ShoppingCartController {
      * Açıklama: Yeni bir alışveriş sepeti oluşturur.
      */
     @PostMapping("")
-    public ShoppingCart createShoppingCart(@RequestBody ShoppingCart shoppingCart) {
-        return shoppingCartService.createShoppingCart(shoppingCart);
+    public ResponseEntity<ShoppingCart> createShoppingCart(@RequestBody ShoppingCart shoppingCart) {
+        ShoppingCart createCart = shoppingCartService.createShoppingCart(shoppingCart);
+        if (createCart != null) {
+            logger.info("Alışveriş sepeti başarıyla oluşturuldu.");
+            return ResponseEntity.ok(createCart);
+        }
+        logger.info("Alışveriş sepeti Bulunamadi");
+        return ResponseEntity.notFound().build();
     }
     // Belirtilen cartId'ye sahip alışveriş sepetini günceller.
 
     @PutMapping("/{cartId}")
-    public ShoppingCart updateShoppingCart(@PathVariable int cartId, @RequestBody ShoppingCart shoppingCart) {
-        return shoppingCartService.updateShoppingCart(cartId, shoppingCart);
+    public ResponseEntity<ShoppingCart> updateShoppingCart(@PathVariable int cartId,
+            @RequestBody ShoppingCart shoppingCart) {
+        ShoppingCart updateCart = shoppingCartService.updateShoppingCart(cartId, shoppingCart);
+        if (updateCart != null) {
+            logger.info("Alışveriş sepeti başarıyla güncellendi.");
+            return ResponseEntity.ok(updateCart);
+        }
+        logger.info("Alışveriş sepeti Bulunamadi");
+        return ResponseEntity.notFound().build();
     }
     // Belirtilen cartId'ye sahip alışveriş sepetini siler.
 
     @DeleteMapping("/{cartId}")
-    public boolean deleteShoppingCart(@PathVariable int cartId) {
+    public Object deleteShoppingCart(@PathVariable int cartId) {
         boolean existsShoppingCartId = shoppingCartService.existsShoppingCartId(cartId);
         if (existsShoppingCartId) {
+            logger.info("Alışveriş sepeti silindi.");
             shoppingCartService.deleteShoppingCart(cartId);
-            return true;
+            return ResponseEntity.ok(true);
         }
-        return false;
+        logger.info("Alışveriş sepeti Bulunamadi");
+        return ResponseEntity.notFound().build();
     }
 
 }
--- finalproject/src/main/java/com/btkakademi/finalproject/controller/UserController.java
+++ finalproject/src/main/java/com/btkakademi/finalproject/controller/UserController.java
@@ -2,10 +2,15 @@ package com.btkakademi.finalproject.controller;
 
 import com.btkakademi.finalproject.model.entity.User;
 import com.btkakademi.finalproject.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -13,28 +18,56 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
+
     @GetMapping(value = "/getAll")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        if (users != null) {
+            logger.info("Kullanıcı listeleme başarılı");
+            return ResponseEntity.ok(users);
+        }
+        logger.info("Kullanıcı listeleme başarısız");
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/{userId}")
-    public User getUserById(@RequestBody int userId) {
-        return userService.getUserById(userId);
+    public ResponseEntity<User> getUserById(@RequestBody int userId) {
+        User userById = userService.getUserById(userId);
+        if (userById != null) {
+            logger.info("Kullanıcı bulundu.");
+            return ResponseEntity.ok(userById);
+        }
+        logger.info("Kullanıcı bulunamadı.");
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping(value = "/create")
-    public User createUser(@RequestBody User user) { // userID de istiyor sıkıntı mı ?
-        return userService.createUser(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createUser = userService.createUser(user);
+        if (createUser != null) {
+            logger.info("Kullanıcı oluşturuldu.");
+            return ResponseEntity.ok(createUser);
+        }
+        logger.info("Kullanıcı oluşturulamadı.");
+        return ResponseEntity.notFound().build();
     }
 
     @PutMapping("/{userId}")
-    public User updateUser(@PathVariable int userId, @RequestBody User user) {
-        return userService.updateUser(userId, user);
+    public ResponseEntity<User> updateUser(@PathVariable int userId, @RequestBody User user) {
+        User updateUser = userService.updateUser(userId, user);
+        if (updateUser != null) {
+            logger.info("Kullanıcı güncellendi.");
+            return ResponseEntity.ok(updateUser);
+        }
+        logger.info("Kullanıcı güncellenemedi.");
+        return ResponseEntity.notFound().build();
+
     }
 
     @DeleteMapping("/delete/{userId}")
-    public void deleteUser(@PathVariable int userId) { // , @RequestParam String password eklenecek spring ssecurityde
+    public void deleteUser(@PathVariable int userId) {
+        logger.info("Kullanıcı silindi.");
         userService.deleteUser(userId);
     }
 }
